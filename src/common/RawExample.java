fec3ed2bbabe41dea0bd3dde45bba8039dfa7db7
Xunhu(Tiger) Sun <sunx2013@my.fit.edu>
2014-06-30 20:01:35 -0400

    +Override equal of RawExample

--- src/common/RawExample.java
+++ src/common/RawExample.java
@@ -15,6 +15,40 @@ public class RawExample {
     public String t = null;
 
     @Override
+    public boolean equals(Object o){
+        if (!(o instanceof RawExample)){
+            return false;
+        }
+        final RawExample e = (RawExample)o;
+        if (this.xList.size() != e.xList.size()) {
+            return false;
+        }
+        for (int i = 0; i < xList.size(); i ++){
+            if (!this.xList.get(i).equals(e.xList.get(i))){
+                return false;
+            }
+        }
+        if (this.t == null && e.t != null){
+            return false;
+        }
+        if (!this.t.equals(e.t)){
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int p = 101;
+        int hash = 5;
+        for (String s: xList){
+            hash = p * hash + (s != null ? s.hashCode() : 0);
+        }
+        hash = p * hash + (t != null ? t.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
     public String toString () {
         final StringBuffer sb = new StringBuffer();
         //sb.append("X: ");
